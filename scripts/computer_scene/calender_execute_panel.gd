099e124a33cfd434529765c48bac0c3a54aec871
yu <yy1973100@163.com>
2025-10-22 16:03:13 +0800

    interrupt update
    更新移动逻辑，留档，还未加入移动动效

---
+++ scripts/computer_scene/calender_execute_panel.gd
@@ -0,0 +1,40 @@
+extends Panel
+
+var card_count : int = 0
+
+var card_width : float = 150
+var self_width : float = 0
+
+func _ready() -> void:
+	self_width = self.size.x
+
+func _on_child_entered_tree(node: Node) -> void:
+	card_count += 1
+	if card_width * card_count > self_width:
+		call_deferred("reset_all_position")
+	else:
+		call_deferred("reset_card_position",node)
+
+
+func _on_child_exiting_tree(node: Node) -> void:
+	card_count -= 1
+	if card_width * card_count > self_width:
+		call_deferred("reset_all_position")
+	else:
+		call_deferred("leave_reset")
+
+func reset_all_position():
+	for i in range(card_count) :
+		var current_card : Card = get_child(i)
+		current_card.position.x = i * self_width / card_count
+		current_card.global_position.y = self.global_position.y
+
+func reset_card_position(card : Card) :
+	card.position.x = (card_count-1) * card_width
+	card.global_position.y = self.global_position.y
+
+func leave_reset():
+	for i in range(card_count) :
+		var current_card : Card = get_child(i)
+		current_card.position.x = i * card_width
+		current_card.global_position.y = self.global_position.y
