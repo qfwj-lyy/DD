8a5af0c6b162a58876b3ed41a744bc9029cdec64
45LightSide <2042467140@qq.com>
2025-10-28 23:58:30 +0800

    222

--- scripts/computer_scene/project_detail_scene.gd
+++ scripts/computer_scene/project_detail_scene.gd
@@ -17,14 +17,15 @@ var project_button
 
 func set_project_information(project : Project):
 	displayed_project = project
+	project_icon.texture = project.icon
 	project_name_label.text = "项目名称：" + project.project_name
-	initial_progress_label.text = "项目初始进度：" + str(project.initial_progress)
-	initial_bug_amount_label.text = "项目初始bug数：" + str(project.initial_bug_amount)
-	bug_amount_limit_label.text = "用户对bug的忍耐度：" + str(project.bug_amount_limit)
-	initial_money_label.text = "初始资金：" + str(project.initial_money)
-	initial_mood_label.text = "参与意向：" + str(project.initial_mood)
-	initial_skill_label.text = "项目内容熟练度：" + str(project.initial_skill)
-	deposit_reward_label.text = "项目总收入：" + str(project.deposit_reward)
+	initial_progress_label.text = "项目初始进度：" + str(int(project.initial_progress))
+	initial_bug_amount_label.text = "项目初始bug数：" + str(int(project.initial_bug_amount))
+	bug_amount_limit_label.text = "用户对bug的忍耐度：" + str(int(project.bug_amount_limit))
+	initial_money_label.text = "初始资金：" + str(int(project.initial_money))
+	initial_mood_label.text = "参与意向：" + str(int(project.initial_mood))
+	initial_skill_label.text = "项目内容熟练度：" + str(int(project.initial_skill))
+	deposit_reward_label.text = "项目总收入：" + str(int(project.deposit_reward))
 	description_label.text = project.description
 
 func _on_project_button_pressed() -> void:
